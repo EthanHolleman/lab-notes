6665e8ce12609798381b204d04253dacd1ec0529
ethan <eholleman@luc.edu>
2021-05-03 12:27:44 -0700

    add labeled gel image

---
+++ transformations/pMAL/images/energy.py
@@ -0,0 +1,9 @@
+
+BP_ENERGY_DICT = []
+
+def energy_crawl_one_base(previous_value, previous_base, next_base):
+    pair = previous_base + next_base
+    return previous_value + BD_ENERGY_DICT[pair]
+
+
+def 
