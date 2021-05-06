370aa94f86a5c321414e6f46c79c8cda4c3ecb59
ethan <eholleman@luc.edu>
2021-05-05 17:45:44 -0700

    add gel image

---
+++ tex/tex_agg.py
@@ -0,0 +1,9 @@
+# aggregate all md files
+
+def modify(parent_folder):
+    # agg all files in parent folder
+    # increase the number of # in each heading
+    # by two
+    # add date as the first heading
+    # concatenate all in date order
+
