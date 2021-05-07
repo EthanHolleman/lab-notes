157a6ca2c47e06f194ed05b330ce1da486c2f517
ethanholleman <etholleman@ucdavis.edu>
2021-05-06 22:13:57 -0700

    add doc strings

--- tex/tex_agg.py
+++ tex/tex_agg.py
@@ -3,33 +3,56 @@ import os
 from pathlib import Path
 from datetime import datetime
 
-def modify(md_file, temp_dir):
-    # agg all files in parent folder
-    # increase the number of # in each heading
-    # by two
-    # add date as the first heading
-    # concatenate all in date order
-    # each dir in the experiments folder should become a chapter
-    # make all paths to images absolute
-    # change links to documents and stuff to link on the github
-    # repo
-    pass
-
 
 def collect_chapters(experiment_dir):
+    '''Collect dirs located in the experiment dir. Each of these
+    will become a chapter in the final output. 
+
+    Args:
+        experiment_dir (Path): Path to experiment directory.
+
+    Returns:
+        dict: Dictionary mapping chapter directories to lists of
+              markdown files. The markdown files should be titled
+              with a mm-dd.yy date and represent the work done
+              on one day pertaint to the name of the parent
+              dir.
+
+    '''
     chapters = {}
     for each_chapter in Path(experiment_dir).iterdir():
-        chapters[each_chapter] = [filepath for filepath in each_chapter.iterdir() 
+        chapters[each_chapter] = [filepath for filepath in each_chapter.iterdir()
                                   if filepath.suffix == '.md'
-                                ]
+                                  ]
     return chapters
 
 
 def make_chapter_yml_header(chapter_name):
+    '''Get a yml formated header for a concatenated
+    chapter file. This info if used by pandoc to
+    correctly assign chapter names.
+
+    Args:
+        chapter_name (str): Name of chapter.
+
+    Returns:
+        str: Yml header as a string.
+    '''
     return '\n'.join(['---', f"title: '{chapter_name}'", '---\n'])
 
 
 def concatendate_chapter_content(temp_dir, chapter_name, chapter_md_files):
+    '''Concatenate individual work day markdown files into one larger
+    chapter markdown files. Files will be ordered by their date (title).
+
+    Args:
+        temp_dir (str): Temp directory to write concat file to.
+        chapter_name (str): Name of chapter.
+        chapter_md_files (list): List of Paths to work day markdown files.
+
+    Returns:
+        Path: Path to concatenated chaper file.
+    '''
     chapter_md_files = sorted(chapter_md_files,
                               key=lambda p: datetime.strptime(p, '%m-%d-%y')
                               )
@@ -42,8 +65,15 @@ def concatendate_chapter_content(temp_dir, chapter_name, chapter_md_files):
             with open(fname) as infile:
                 for line in infile:
                     outfile.write(line)
-    
+
     return concat_file
+
+
+def concatenated_chapter_to_tex(concat_md, output_dir, chapter_template):
+    chapter_tex = concat_md.with_suffix('.tex')
+    cmd = f'pandoc -f markdown -r latex --template={chapter_template} {concat_md} {chapter_tex}'
+    os.system(cmd)
+    return chapter_tex
 
 
 def add_chapters_to_book(chapter_concat_files, main_template, temp_dir):
@@ -68,14 +98,6 @@ def convert_to_pdf(output_dir, notebook_tex):
 def main():
     pass
 
+
 if __name__ == '__main__':
     main()
-
-
-
-
-    
-    
-    
-
-
